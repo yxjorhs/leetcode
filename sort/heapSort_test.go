0d0f98c64442d1cedd88ca899a5b6789a553c426
yangxiaojie <yangxiaojie@tuzhanai.com>
2021-04-07 17:55:25 +0800

    chore: temp

---
+++ sort/heapSort_test.go
@@ -0,0 +1,25 @@
+package sort
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHeapSort(t *testing.T) {
+	assert.Equal(t, []int{1, 2, 3, 4, 6, 7}, headSort([]int{3, 4, 2, 1, 6, 7}))
+}
+
+func BenchmarkHeapSort(b *testing.B) {
+	arr := []int{}
+
+	for i := 0; i < 10000; i++ {
+		arr = append(arr, i+1)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		headSort(arr)
+	}
+}
