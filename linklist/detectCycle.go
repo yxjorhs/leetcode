9476c21b16c2d073e6965b76ac19a533c0bafeb4
yxjorhs <2902468225@qq.com>
2021-06-11 13:55:37 +0800

    chore: 以空间复杂度1判断是否为环形链表

--- linklist/detectCycle.go
+++ linklist/detectCycle.go
@@ -1,20 +1,35 @@
 package linklist
 
 /* detectCycle 以空间复杂度1判断是否为环形链表
- * 遍历链表，判断每个链表是否在循环内，使用快慢指针判断是否循环
+ * 从先向后查找每个结点是否会重复出现，是则为目标
+ * 每次查找通过快慢指针判断是否完成链表的遍历
  */
 func detectCycle(head *ListNode) *ListNode {
 	for head != nil {
-		slow := head.Next
-		fast := head.Next.Next
+		slow := head
+		fast := head
 
-		for slow.Next != fast.Next {
+		isCycle := false
+
+		for fast.Next != nil && fast.Next.Next != nil {
 			slow = slow.Next
 			fast = fast.Next.Next
 
-			if fast == head || slow == head {
+			// 找到重复点
+			if fast == head {
 				return head
 			}
+
+			// 已完成遍历，且是循环链表
+			if slow == fast {
+				isCycle = true
+				break
+			}
+		}
+
+		// 不是循环链表
+		if isCycle == false {
+			return nil
 		}
 
 		head = head.Next
