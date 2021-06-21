4fa1248f0c0497aa17df853c161b5ff97b5f5951
yxjorhs <2902468225@qq.com>
2021-06-21 17:26:55 +0800

    chore: maxProfit2

--- toGroup/maxProfit.go
+++ toGroup/maxProfit.go
@@ -1,5 +1,6 @@
 package togroup
 
+/*maxProfit 最佳卖卖时机，只能买入，卖出1次 */
 func maxProfit(prices []int) int {
 	maxPro := 0
 	minPri := -1
@@ -13,6 +14,24 @@ func maxProfit(prices []int) int {
 		if prices[i]-minPri > maxPro {
 			maxPro = prices[i] - minPri
 		}
+	}
+
+	return maxPro
+}
+
+/*maxProfit 最佳卖卖时机，可买入，卖出多次 */
+func maxProfit2(prices []int) int {
+	maxPro := 0
+	cost := -1 // 买入成本
+
+	for i := 0; i < len(prices); i++ {
+		if cost == -1 || cost > prices[i] {
+			cost = prices[i]
+			continue
+		}
+
+		maxPro += prices[i] - cost
+		cost = prices[i]
 	}
 
 	return maxPro
