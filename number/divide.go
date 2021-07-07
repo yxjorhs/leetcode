fd51f5140c9f0ca00e2f1c48780eb3e965ba8f47
yxjorhs <2902468225@qq.com>
2021-07-07 18:32:36 +0800

    chore: update

--- number/divide.go
+++ number/divide.go
@@ -3,43 +3,47 @@ package number
 const maxInt = 2147483647
 
 func divide(dividend int, divisor int) int {
-	arr := []int{} // 二进制数组，arr[0]代表2的0次幂是1
-
-	for 
-
-	if abs(dividend) < abs(divisor) {
-		return 0
-	}
-
-	sum := divisor
-	cou := 1
+	resSign := 1
 
 	if dividend>>31 != divisor>>31 {
-		cou = -1
+		resSign = -1
 	}
 
-	for {
-		if abs(sum+sum) > abs(dividend) {
-			break
+	res := 0
+	dividend = abs(dividend)
+	divisor = abs(divisor)
+
+	arr := []int{} // 二进制数组，arr[0] = 10代表(2^0)个divisor的和是10
+	for i, sum := 0, divisor; sum <= dividend; i += i {
+		arr = append(arr, sum)
+		sum += sum
+	}
+
+	for dividend >= divisor {
+		divisorCount := 1
+		sum := arr[0]
+
+		for i := 1; i < len(arr); i++ {
+			if arr[i] > dividend {
+				break
+			}
+			divisorCount += divisorCount
+			sum = arr[i]
 		}
 
-		sum += sum
-		cou += cou
+		res += divisorCount
+		dividend -= sum
 	}
 
-	leave := abs(dividend) - abs(sum)
-
-	if dividend < 0 {
-		leave = 0 - leave
+	if resSign == -1 {
+		return 0 - res
 	}
 
-	ret := cou + divide(leave, divisor)
-
-	if ret > maxInt {
+	if res > maxInt {
 		return maxInt
 	}
 
-	return ret
+	return res
 }
 
 func abs(num int) int {
